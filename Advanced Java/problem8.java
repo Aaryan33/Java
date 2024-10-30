de736257905335731944dd36f54c86c1ced026e4
Aaryan Patel <97274994+Aaryan33@users.noreply.github.com>
2024-10-30 13:13:27 +0530

    Add files via upload

---
+++ Advanced Java/problem8.java
@@ -0,0 +1,11 @@
+// Write a program to create a Dialogbox.
+
+import javax.swing.*;
+
+public class problem8 {
+    public static void main(String[] args) {
+        JDialog a = new JDialog();
+        a.setVisible(true);
+        a.setSize(300, 300);
+    }
+}
