296c7fb3d0b0289f4b5ecc4dbd55c7ab10ae10d5
Aaryan Patel <97274994+Aaryan33@users.noreply.github.com>
2024-11-04 17:34:40 +0530

    Add files via upload

---
+++ module_4_problem13.java
@@ -0,0 +1,11 @@
+// Write a program to demonstrate System clock.
+import java.time.Clock;
+import java.time.Duration;
+public class module_4_problem13 {
+    public static void main(String[] args) {
+      Clock c = Clock.systemUTC();
+      Duration d = Duration.ofHours(5);
+      Clock clock = Clock.offset(c, d);
+      System.out.println(clock.instant());
+  }
+}
